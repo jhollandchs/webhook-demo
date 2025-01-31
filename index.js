e20e8c9cdfc6f544217a372f93782b06488974b1
Jason Holland <Jason.Holland@chsinc.com>
2025-01-31 08:45:31 -0600

    update port

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 const http = require('http');
 
 // Define the port to listen on
-const port = 3000;
+const port = 443;
 
 // Create the HTTP server
 const server = http.createServer((req, res) => {
